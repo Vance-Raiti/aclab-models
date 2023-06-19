75ea31cb56c584955dc6e0dcb4d53ed3e0b404d1
Vance-Raiti <vraiti77@gmail.com>
2023-06-19 15:52:58 -0400

    updated for new Trainer object

--- enformer/train.py
+++ enformer/train.py
@@ -1,79 +1,94 @@
 import torch
-from dnaset.util import seq_to_onehot
-import time
-from enformer_pytorch import Enformer, str_to_one_hot
+from enformer_pytorch import Enformer
 from enformer_pytorch.config_enformer import EnformerConfig
-import sys
-sys.path.append("/projectnb/aclab/datasets/dataloaders/dataloaders/basenji")
 from basenji import BasenjiDataset
-from torch.utils.data import DataLoader
 from filewriter import write
-from torch.optim import Adam
-print("building model")
-'''
+from olltrainer.trainer import Trainer, Writer, TrainerConfig
+import torch.optim as optim
+import time
+import wandb
+import os
+
+
+BATCH_SIZE = 1
+EPOCHS = 5
+MAX_LR = 1e-2
+log_pth = "logs/write.out"
+os.remove(log_pth)
+
+class EnformerWriter(Writer):
+	def __init__(self):
+		self.running_loss = 0.0
+		self.it = 0
+		self.running_r = 0.0 #correlation coefficient
+		self.last_time = time.monotonic()
+		self.last_it = 0
+		self.running_out = 0.0
+
+	def record(self,model_output):
+		self.running_loss += model_output['loss'].item()
+		self.it += 1
+		self.running_r += model_output['correlation_coefficient'].item()
+		self.running_out += torch.mean(model_output['loss']).item()	
+
+	def write(self):
+		nits = self.it-self.last_it
+		msg = f"{self.it} -- avg loss: {self.running_loss/nits} avg r: {self.running_r/nits} avg output: {self.running_out/nits} it/sec: {nits*BATCH_SIZE/(time.monotonic()-self.last_time)}\n"
+		print(msg)
+		write(msg,log_pth)
+		
+		self.running_loss = 0.0
+		self.running_r = 0.0
+		self.running_out = 0.0
+		self.last_time = time.monotonic()
+		self.last_it = self.it
+
+
 pred = Enformer.from_hparams(
 	dim = 1536,
 	depth = 11,
 	heads = 8,
 	output_heads = dict(human = 5313),
 	target_length = 896,
-	use_checkpointing=True
-
+	use_checkpointing = True
 )
-'''
-pred = Enformer.from_pretrained('EleutherAI/enformer-official-rough')
 
-print("model built")
-model_pth = "/projectnb/aclab/vraiti/models/enformer/save/params"
-if len(sys.argv)==2:
-	prev_its = int(sys.argv[1])
-	model_pth+=str(prev_its)+".pt"
-elif len(sys.argv)>2:
-	print("usage: train.py [specific checkpoint]")
-	exit()
-else:
-	model_pth+=".pt"
+optimizer = optim.Adam(
+    pred.parameters(),
+    lr=5e-4,
+)
 
+lr_scheduler= optim.lr_scheduler.LambdaLR(
+    optimizer,
+    lambda n: min(n*MAX_LR/5000,MAX_LR)
+)
 
-
-#pred.load_state_dict(torch.load(model_pth))
 data = BasenjiDataset(
 	organism= "human",
 	split = "train",
 	seq_length=114688
 )
+        
 
-optimizer = Adam(pred.parameters())
-batch_size = 1
-dl = DataLoader(data,batch_size=batch_size,shuffle=False)
-last_time = time.monotonic()
+train_conf = TrainerConfig(
+	ckpt_pth = "save/enformer",
+	write_interval = 100,
+    save_interval = 1000,
+	batch_size = BATCH_SIZE,
+	writer = EnformerWriter(),
+    forward_targets = True,
+    use_model_loss = True,
+    use_autocast = False,
+    epochs=EPOCHS,
+    max_grad_norm=0.2
+)
 
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-#pred = torch.compile(pred,mode="reduce-overhead")
-pred = pred.to(device)
+train_obj = Trainer(
+	model = pred,
+	config = train_conf,
+	optimizer = optimizer,	
+	train_data = data
+    lr_scheduler = lr_scheduler
+)
 
-for it, seq_dict in enumerate(dl):
-    bpstr = seq_dict['sequence'] #base pairs
-    x = str_to_one_hot(bpstr)
-    x = x.to(device)
-    y = seq_dict['target'].to(device)
-    ret = pred(
-		x=x,
-		target=y,
-		return_corr_coef=True,
-		head='human'
-	)
-    loss = ret['loss']
-    coef = ret['corr_coef']
-
-    loss.backward()
-    optimizer.step()
-
-    msg=f"loss: {loss}, corr coef: {coef}, it/second: {batch_size/(time.monotonic()-last_time)}\n:"
-    print(msg)
-    write("train.py.out",msg)
-
-    last_time = time.monotonic()
-    if it%20==0:
-        torch.save(pred.state_dict(),f"/projectnb/aclab/vraiti/models/enformer/save/params{it}.pt")
-torch.save(pred.state_dict(),f"/projectnb/aclab/vraiti/models/enformer/save/params.pt")
+train_obj.train()
